beb9ac9b0606b64a4b9f8aa9675942598dc7fe0b
Kejsi Keta <105013954+kc888k@users.noreply.github.com>
2022-05-05 22:11:35 +0200

    Add files via upload

---
+++ LAB1 usht5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a,b,siperfaqe,perimetri;
+   a=5;
+   b=10;
+   siperfaqe=a*b;
+   perimetri=2*a+2*b;
+    printf("\nsiperfaqja e drejtkendeshit=%d",siperfaqe);
+    printf("\nperimetri i drejtkendeshit=%d",perimetri);
+}
