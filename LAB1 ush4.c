87cdd8532da270ad621683f2cdcc4ce7232437ce
Kejsi Keta <105013954+kc888k@users.noreply.github.com>
2022-05-05 22:10:26 +0200

    Add files via upload

---
+++ LAB1 ush4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int brinje,siperfaqe,perimetri;
+   brinje=5;
+   siperfaqe=brinje*brinje;
+   perimetri=4*brinje;
+    printf("\nsiperfaqja e katrorit=%d",siperfaqe);
+    printf("\nperimetri i katrorit=%d",perimetri);
+}
